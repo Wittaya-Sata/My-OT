528d49c5749135ed2575f460a641be894c25d9c6
Wittaya-Sata <wittaya.sata.02@gmail.com>
2025-07-17 11:28:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const summaryMonth = document.getElementById('summaryMonth');
 const summaryYear = document.getElementById('summaryYear');
 const otChart = document.getElementById('otChart');
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbylPykS0nClQHhDM0-3CTYZ7QLZ02VKjRQUYcuSMRp6yl72Ql_yj9IeT19BbjvE4LsKOQ/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwxuTyehHP1bOt_R--GYRWKWUKJ9Hs0EB51_iID_PMdHY5VvLr40mHfFXSxkXeUXpk/exec';
 let otData = [];
 
 function calcHours(start, end) {
